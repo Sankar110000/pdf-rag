8e23655e0693cf8eb2d00dfc445ec1d01a69afe0
Sankar110000 <skaibalya748@gmail.com>
2026-08-30 21:35:54 +0530

    Syntax error fixed

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,7 +3,10 @@ import saveFile from "@/helper/savefile";
 
 export async function POST(request: Request) {
   try {
-    const file: = (await request.formData()).get("pdf") as File | null;
+    const file = (await request.formData()).get("pdf") as File | null;
+    if(!file){
+      return Response.json({success: false, message: "Please provide a file"})
+    }
     const filename = await saveFile(file);
     await indexFile(filename?.filename);
     return Response.json({ message: "working" });
