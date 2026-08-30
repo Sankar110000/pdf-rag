1e1354af2d77ed79a7b4e022dbad45b54d3a701c
Sankar110000 <skaibalya748@gmail.com>
2026-08-30 21:47:31 +0530

    fixed types in route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,16 @@
 import indexFile from "@/helper/indexing";
 import saveFile from "@/helper/savefile";
+import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
     const file = (await request.formData()).get("pdf") as File | null;
     if(!file){
-      return Response.json({success: false, message: "Please provide a file"})
+      return NextResponse.json({success: false, message: "Please provide a file"})
     }
     const filename = await saveFile(file);
     await indexFile(filename?.filename);
-    return Response.json({ message: "working" });
+    return NextResponse.json({ message: "working" });
   } catch (error) {
     console.log(error);
   }
